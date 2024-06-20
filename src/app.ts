5fa8e7bc3a157d016a7bd32936433a61faf6e6b8
enayetaitech <mdrehman@365aitech.com>
2024-06-20 19:39:03 +0600

    cors changed

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ const app: Application = express();
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:3000'] }));
+app.use(cors({ origin: '*' }));
 
 // application routes
 app.use('/api/', router);
