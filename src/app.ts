d27048e118879123590071448f6821f21cb7a762
enayetaitech <mdrehman@365aitech.com>
2024-09-18 21:30:02 +0600

    new cors added

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ const app: Application = express();
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:5000'] }));
+app.use(cors({ origin: ['http://localhost:5173'] }));
 
 // application routes
 app.use('/api/', router);
